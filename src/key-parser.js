89f682f5b480ac799247a51e0cd9989d3d2265cb
Alex Kolpa <akolpa1990@gmail.com>
2017-05-24 12:27:57 +0200

    Improve readability key parsing
    Extracted the specifier construction to a separate function to allow for a bit more documentation. Also fixed function naming.

--- src/key-parser.js
+++ src/key-parser.js
@@ -13,37 +13,47 @@ function parseKey(key, prefix) {
 		const specifierStart = tail.indexOf('/[');
 		const serviceName = tail.substring(0, specifierStart);
 		const specifierEnd = tail.indexOf(']');
+		const specifiersPart = tail.substring(specifierStart + 2, specifierEnd);
 
-		const specifiers = tail.substring(specifierStart + 2, specifierEnd).split(',')
+		const specifiers = extractSpecifiers(specifiersPart);
+
+		tail = tail.substring(specifierEnd + 1);
+
+		return Object.assign({name : serviceName, key : tail}, ...specifiers);
+	}
+	else if (tail.includes('/')) {
+		const serviceName = tail.substring(0, tail.indexOf('/'));
+		tail = tail.substring(tail.indexOf('/') + 1);
+		return {key : tail, name : serviceName};
+	}
+	else {
+		return {key : '', name : tail};
+	}
+}
+
+/**
+ * Splits up the specifier string into an array of specifier objects
+ * @param {string} specifiersPart - A string of the following expected format: [spec1=val1,spec2=val2,...]
+ * @return {Array} An array of specifiers in the following format [{dataCenter:'development'},{host:'localhost'},...]
+ */
+function extractSpecifiers(specifiersPart) {
+	return specifiersPart.split(',')
 			.map(specifier => specifier.split('='))
 			.filter(kvArray => validSpecifiers.hasOwnProperty(kvArray[0]))
 			.map(kvArray => {
 				const key = validSpecifiers[kvArray[0]];
 				return {[key] : kvArray[1]};
 			});
-
-		tail = tail.substring(specifierEnd + 1);
-
-		return Object.assign({serviceName, key : tail}, ...specifiers);
-	}
-	else if (tail.includes('/')) {
-		const serviceName = tail.substring(0, tail.indexOf('/'));
-		tail = tail.substring(tail.indexOf('/') + 1);
-		return {key : tail, serviceName};
-	}
-	else {
-		return {key : '', serviceName : tail};
-	}
 }
 
 function keyApplies(key, service) {
-	return isSetAndEqual(key.name, service.name)
-		&& isSetAndEqual(key.dataCenter, service.dataCenter)
-		&& isSetAndEqual(key.host, service.host)
-		&& isSetAndEqual(key.instance, service.instance);
+	return isUnsetOrEqual(key.name, service.name)
+		&& isUnsetOrEqual(key.dataCenter, service.dataCenter)
+		&& isUnsetOrEqual(key.host, service.host)
+		&& isUnsetOrEqual(key.instance, service.instance);
 }
 
-function isSetAndEqual(keyField, serviceField) {
+function isUnsetOrEqual(keyField, serviceField) {
 	return !keyField || keyField === serviceField;
 }
 
