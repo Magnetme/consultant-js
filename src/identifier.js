288985b2b9d8d74492d0c7b6d7056f7587d76a70
Alex Kolpa <akolpa1990@gmail.com>
2020-04-05 16:26:09 +0200

    Upgrade dependencies
    This updates all dependencies to their latest versions. Verified with our React app.

--- src/identifier.js
+++ src/identifier.js
@@ -1,5 +1,5 @@
 import fetchAgent from './agent';
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 export default async function loadIdentifier(identifier, consulHost) {
 	const agent = await fetchAgent(consulHost);
@@ -8,6 +8,6 @@ export default async function loadIdentifier(identifier, consulHost) {
 		name : identifier.name || process.env.SERVICE_NAME,
 		dataCenter : identifier.dataCenter || process.env.SERVICE_DC || agent.Config.Datacenter,
 		host : identifier.host || process.env.SERVICE_HOST || agent.Config.NodeName,
-		instance : identifier.instance || process.env.SERVICE_INSTANCE || uuid.v4()
+		instance : identifier.instance || process.env.SERVICE_INSTANCE || uuidv4(),
 	};
 }
