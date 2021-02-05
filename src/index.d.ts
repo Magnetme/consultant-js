ecb36789a6523fdf6042ffbf5e207456234244b4
Alex Kolpa <akolpa1990@gmail.com>
2021-02-05 14:38:44 +0100

    Add TypeScript definitions

---
+++ src/index.d.ts
@@ -0,0 +1,40 @@
+declare module "@magnet.me/consultant" {
+
+    type Properties = Record<string, string>;
+
+	interface ServiceIdentifier {
+		name: string;
+		dataCenter: string;
+		host: string;
+		instance: string;
+	}
+
+	interface ServiceArguments {
+		service: { name: string, port: number };
+		healthCheckPath: string;
+		healthCheckInterval?: number;
+		consulHost?: string;
+		prefix?: string;
+	}
+
+	interface Service {
+		identifier: ServiceIdentifier;
+		deregister: () => Promise<void>;
+	}
+
+	interface ConfigArguments {
+		consulHost?: string;
+		service: Service;
+		prefix?: string;
+	}
+
+	interface Config {
+		getProperties(): Properties;
+		register(callback: (Properties) => void): void;
+		deregister(callback: (Properties) => void): void;
+		stop(): void;
+	}
+
+	function service(args: ServiceArguments): Promise<Service>;
+	function config(args: ConfigArguments): Promise<Config>;
+}
